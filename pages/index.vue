8c48fa4a363366e3ec0cd23754ec58438ae4f9c1
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-10-09 21:24:42 +0300

    base routing

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<h2>Home</h2>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			Odio voluptates cumque animi ipsa, dicta beatae molestias
+			est consequatur accusantium tempore.</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			Odio voluptates cumque animi ipsa, dicta beatae molestias
+			est consequatur accusantium tempore.</p>
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+	margin-bottom: 20px;
+	font-size: 36px;
+}
+
+p {
+	margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Products details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsum quidem delectus temporibus reprehenderit
+      facilis quam eaque aut culpa numquam?</p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped></style>

646f61e53942fdd9ca6e9e24f2ff9c60669e474d
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-10-13 07:40:46 +0300

    feat: add component product-card

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,7 +3,7 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products">
-        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+        <ProductCard :product="p" />
       </div>
     </div>
   </div>
@@ -14,7 +14,7 @@
     layout: 'products'
   })
 
-  // Вызов продукта
+  // Вызов продуктов
   const {data: products} = await useFetch('https://fakestoreapi.com/products')
 </script>
 
